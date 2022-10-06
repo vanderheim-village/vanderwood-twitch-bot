a73753ad4c5c6422efac623ac96c20a233c41be8
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2022-10-06 02:52:11 +0000

    models: Update models and create base migrations.

--- app/models.py
+++ app/models.py
@@ -1,16 +1,18 @@
+from enum import unique
 from tortoise.models import Model
 from tortoise import fields
 
 class Clan(Model):
     id = fields.IntField(pk=True)
-    name = fields.CharField(max_length=255)
-    tag = fields.CharField(max_length=255)
+    name = fields.CharField(max_length=255, unique=True)
+    tag = fields.CharField(max_length=4, unique=True)
 
 
 class Player(Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=255, unique=True)
     clan = fields.ForeignKeyField('models.Clan', related_name='players')
+    enabled = fields.BooleanField(default=True)
 
 
 class Season(Model):
