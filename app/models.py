e6ee685283dca93574e39ddc75adc201235bce4e
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2022-10-06 03:01:58 +0000

    models: Add manager and function check to player model.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from enum import unique
 from tortoise.models import Model
+from tortoise.manager import Manager
 from tortoise import fields
 
 class Clan(Model):
@@ -7,12 +8,20 @@ class Clan(Model):
     name = fields.CharField(max_length=255, unique=True)
     tag = fields.CharField(max_length=4, unique=True)
 
+class StatusManager(Manager):
+    def get_queryset(self):
+        return super(StatusManager, self).get_queryset().filter(enabled=True)
 
 class Player(Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=255, unique=True)
     clan = fields.ForeignKeyField('models.Clan', related_name='players')
     enabled = fields.BooleanField(default=True)
+
+    enabled_players = StatusManager()
+
+    def is_enabled(self) -> bool:
+        return self.enabled
 
 
 class Season(Model):
