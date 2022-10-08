25becd7a630f02c0682949702ed903e955cb3f31
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2022-10-08 21:14:12 +0000

    commands: Add checkin command.

--- app/modules/cogs/bom_commands.py
+++ app/modules/cogs/bom_commands.py
@@ -1,6 +1,6 @@
 from twitchio.ext import commands
 
-from app.models import Clan, Player, Points, Season, Checkin
+from app.models import Checkin, Player, Points, Season, Session
 
 
 class BomCommandsCog(commands.Cog):
@@ -62,7 +62,38 @@ class BomCommandsCog(commands.Cog):
         !checkin command
         """
         if await Season.active_seasons.all().exists():
-            pass
+            season = await Season.active_seasons.first()
+            if await Session.active_session.all().exists():
+                session = await Session.active_session.first()
+                if await Player.get_or_none(name=ctx.author.name):
+                    player = await Player.get(name=ctx.author.name)
+                    if player.is_enabled() and player.clan:
+                        if await Checkin.get_or_none(player=player, session=session):
+                            await ctx.send(f"@{ctx.author.name} has already checked in!")
+                        else:
+                            await Checkin.create(player=player, session=session)
+                            clan = await player.clan.get()
+                            if await Points.get_or_none(player=player, season=season):
+                                points = await Points.get(player=player, season=season)
+                                points.points += 100
+                                await points.save()
+                            else:
+                                assert player.clan is not None
+                                await Points.create(
+                                    player_id=player.id,
+                                    season_id=season.id,
+                                    points=100,
+                                    clan_id=clan.id,
+                                )
+                            await ctx.send(f"@{ctx.author.name} has checked in!")
+                    else:
+                        await ctx.send(f"@{ctx.author.name} is not in a Clan roster!")
+                else:
+                    await ctx.send(f"@{ctx.author.name} is not in a clan roster!")
+            else:
+                await ctx.send("No active session!")
+        else:
+            await ctx.send("No active seasons!")
 
     @commands.command()
     async def myrewards(self, ctx: commands.Context) -> None:
