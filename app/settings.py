97b07d2f4140cd26968a8739436c5ec983c579f3
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2023-01-19 18:01:42 +0000

    config: Allow for different TEST and PROD databases.

--- app/settings.py
+++ app/settings.py
@@ -3,7 +3,13 @@ import yaml
 with open("config.yaml", "r") as stream:
     config_options = yaml.safe_load(stream)
 
-database_config = config_options["APP"]["DATABASE"]
+if config_options["APP"]["MODE"] == "TEST":
+    database_config = config_options["APP"]["TEST_DATABASE"]
+elif config_options["APP"]["MODE"] == "PROD":
+    database_config = config_options["APP"]["DATABASE"]
+else:
+    print("Invalid mode, please set to either TEST or PROD.")
+    exit(1)
 
 TORTOISE = {
     "connections": {
