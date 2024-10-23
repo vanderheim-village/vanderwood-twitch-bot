d8d977b6c77cf073efb39c433e29d1e97cdb38b1
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2024-10-23 23:34:30 +0000

    clients: Create client for follower giveaway entries endpoints.

--- app/clients/vanderheim/main_client.py
+++ app/clients/vanderheim/main_client.py
@@ -3,6 +3,7 @@ from app.clients.vanderheim.endpoints.checkins import CheckinsAPI
 from app.clients.vanderheim.endpoints.clan_spoils_claims import ClanSpoilsClaimsAPI
 from app.clients.vanderheim.endpoints.clan_spoils_sessions import ClanSpoilsSessionsAPI
 from app.clients.vanderheim.endpoints.clans import ClansAPI
+from app.clients.vanderheim.endpoints.follower_giveaway_entries import FollowerGiveawayEntriesAPI
 
 
 class VanderheimAPIClient:
@@ -11,4 +12,5 @@ class VanderheimAPIClient:
         self.checkins = CheckinsAPI(self.base_client)
         self.clan_spoils_claims = ClanSpoilsClaimsAPI(self.base_client)
         self.clan_spoils_sessions = ClanSpoilsSessionsAPI(self.base_client)
-        self.clans = ClansAPI(self.base_client)      
+        self.clans = ClansAPI(self.base_client)
+        self.follower_giveaway_entries = FollowerGiveawayEntriesAPI(self.base_client)
